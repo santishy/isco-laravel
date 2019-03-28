d2bfacc85ffce4c668fc7a563b153f7ec2675a30
vagrant <vagrant@homestead>
2019-03-28 04:58:24 +0000

    aa-

--- config/queue.php
+++ config/queue.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_DRIVER', 'database'),
+    'default' => env('QUEUE_DRIVER', 'sync'),
 
     /*
     |--------------------------------------------------------------------------
@@ -31,7 +31,7 @@ return [
     'connections' => [
 
         'sync' => [
-            'driver' => 'database',
+            'driver' => 'sync',
         ],
 
         'database' => [
