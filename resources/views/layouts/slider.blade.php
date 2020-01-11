55345d86ac6b442d4a9e95860c88525f3e6d9f30
Santiago <santi_shy@hotmail.com>
2020-01-11 12:43:00 -0600

    slider

--- resources/views/layouts/slider.blade.php
+++ resources/views/layouts/slider.blade.php
@@ -1,44 +1,37 @@
 @section('slider')
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1">
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-          <!-- Indicators -->
-          <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-          </ol>
-          <!-- Wrapper for slides -->
-          <div class="carousel-inner" role="listbox">
-            @php $ban=true; @endphp
-            @foreach($sliders as $slider)
-            <div class="item @php if($ban) echo 'active'; $ban=false; @endphp">
-              <img src="{{asset('storage/sliders/'.$slider->name.'.'.$slider->extension)}}" alt="...">
-              <div class="carousel-caption">
-              </div>
+    <div class="row justify-content-center">
+      <div class="col-md-10">
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+              <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              @foreach($sliders as $key=>$slider)
+                @php
+                  $active='';
+                  if(!$key)
+                    $active = 'active';
+                @endphp
+                <div class="{{'carousel-item'.' '.$active}}">
+                  <img src="{{asset('storage/sliders/'.$slider->name.'.'.$slider->extension)}}" class="d-block w-100" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                  </div>
+                </div>
+              @endforeach
             </div>
-            @endforeach
-            {{-- <div class="item">
-              <img src="{{asset('images/s2.jpg')}}" alt="...">
-              <div class="carousel-caption">
-              </div>
-            </div>
-            <div class="item">
-              <img src="{{asset('images/s3.jpg')}}" alt="...">
-              <div class="carousel-caption">
-              </div>
-            </div> --}}
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
           </div>
-          <!-- Controls -->
-          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
       </div>
     </div>
 @endsection
