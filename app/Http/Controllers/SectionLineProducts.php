4f82f50c9b8bdf2821c3538885c3467e5d137f1c
santishy <santiagomartinochoaestrada@gmail.com>
2020-02-13 14:10:11 -0600

    f

--- app/Http/Controllers/SectionLineProducts.php
+++ app/Http/Controllers/SectionLineProducts.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class SectionLineProducts extends Controller
 {
-    //
+    public function index(){
+      $products=Articulo::with('utilidade')->where('id_linea',$id_linea)->where('id_seccion',\Session::get('id_seccion'))->where('activo','=',1)->orderBy('precio','asc')->paginate(20);
+      $ruta = url()->full();
+      $method = 'GET';
+      if($request->wantsJson())
+          return new ProductsCollection($products);
+    }
 }
