7e1c4db90d72589754e9ab4e3214ab86f5e43474
Santiago <santi_shy@hotmail.com>
2020-02-20 18:52:06 -0600

    validando que existe el archivo .env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -81,8 +81,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-	$this->app->bind('path.public',function(){
-	   return base_path().'/public_html';
-	});
+      if(!env('APP_KEY'))
+      	$this->app->bind('path.public',function(){
+      	   return base_path().'/public_html';
+    	});
     }
 }
