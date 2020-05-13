ced62b13dca88a379512157a1db57f652e04832e
Santiago <santi_shy@hotmail.com>
2020-05-13 18:47:00 -0500

    modificando orders

---
+++ resources/js/components/orders/CurrentOrder.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <table>
+        <tbody>
+          <tr>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['shopping_cart_id'],
+  data(){
+    return{
+      order:null,
+      url:'/orders/'+this.shopping_cart_id
+    }
+  },
+  created(){
+    this.getOrder(this.shopping_cart_id);
+  },
+  methods:{
+    getOrder(id){
+      axios({
+        method:'GET',
+        url:this.url,
+        params:{
+          id:this.shopping_cart_id
+        }
+      }).then((res)=>{
+        if(res.data.length){
+          this.order = res.data
+        }
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -5,38 +5,73 @@
 		<div class="row">
 			<div class="col-md-6 margin-top-2" style="margin-bottom: 2em;">
 				<div class="card">
+					<div class="card-header bg-secundary">
+						 <h3 class="card-title mb-0">Datos de Envío</h3>
+					</div>
 				  <div class="card-body">
-				    <h3 class="card-title">Datos de Envío</h3>
+
 					{!! Form::Open(['method'=>'POST', 'url' => 'orders']) !!}
 						<div class="form-group">
-							<label>Domicilio</label>
-							<input type="text" name="line1" class="form-control" placeholder="Calle #numero">
+
+							<input type="text" name="line1" class="form-control" placeholder="Domicilio, ejemplo: Calle #numero">
 						</div>
 						<div class="form-group">
-							<label>Referencia</label>
-							<input type="text" name="line2" class="form-control" placeholder="Calle">
+							<input type="text" name="line2" class="form-control" placeholder="Referencia">
 						</div>
 						<div class="form-group">
-							<label>Estado</label>
-							<input type="text" name="state" class="form-control">
+							<select name="state" class="form-control">
+						      <option value="">Seleccione un estado</option>
+						      <option value="Aguascalientes">Aguascalientes</option>
+						      <option value="Baja California">Baja California</option>
+						      <option value="Baja California Sur">Baja California Sur</option>
+						      <option value="Campeche">Campeche</option>
+						      <option value="Chiapas">Chiapas</option>
+						      <option value="Chihuahua">Chihuahua</option>
+						      <option value="CDMX">Ciudad de México</option>
+						      <option value="Coahuila">Coahuila</option>
+						      <option value="Colima">Colima</option>
+						      <option value="Durango">Durango</option>
+						      <option value="Estado de México">Estado de México</option>
+						      <option value="Guanajuato">Guanajuato</option>
+						      <option value="Guerrero">Guerrero</option>
+						      <option value="Hidalgo">Hidalgo</option>
+						      <option value="Jalisco">Jalisco</option>
+						      <option value="Michoacán">Michoacán</option>
+						      <option value="Morelos">Morelos</option>
+						      <option value="Nayarit">Nayarit</option>
+						      <option value="Nuevo León">Nuevo León</option>
+						      <option value="Oaxaca">Oaxaca</option>
+						      <option value="Puebla">Puebla</option>
+						      <option value="Querétaro">Querétaro</option>
+						      <option value="Quintana Roo">Quintana Roo</option>
+						      <option value="San Luis Potosí">San Luis Potosí</option>
+						      <option value="Sinaloa">Sinaloa</option>
+						      <option value="Sonora">Sonora</option>
+						      <option value="Tabasco">Tabasco</option>
+						      <option value="Tamaulipas">Tamaulipas</option>
+						      <option value="Tlaxcala">Tlaxcala</option>
+						      <option value="Veracruz">Veracruz</option>
+						      <option value="Yucatán">Yucatán</option>
+						      <option value="Zacatecas">Zacatecas</option>
+						  </select>
 						</div>
 						<div class="form-group">
-							<label>Ciudad</label>
-							<input type="text" name="city" class="form-control">
+							<input type="text" name="city" placeholder="Ciudad" class="form-control">
 						</div>
 						<div class="form-group">
-							<label>Codigo Postal</label>
-							<input type="text" name="postal_code" class="form-control">
+							<input type="text" name="postal_code" placeholder="Codigo Postal" class="form-control">
 						</div>
-						<div class="form-group">
-							<label>La persona dirigida</label>
-							<input type="text" name="recipient_name" class="form-control">
+						<div class="form-group mb-0">
+							<input type="text" name="recipient_name" placeholder="Persona dirigida" class="form-control">
 						</div>
-						<div class="form-group">
-							<button class="btn btn-primary">Guardar</button>
+
+				  </div>
+					<div class="card-footer bg-secundary">
+						<div class="form-group mb-0">
+							<button class="btn btn-primary btn-block mb-0">Guardar</button>
 						</div>
 					{!! Form::close() !!}
-				  </div>
+					</div>
 				</div>
 
 				@if ($errors->any())
